5a725574e5fc19b7ca8d9e6505a6048953925c6e
Marina Guimarães <marinagvieira95@gmail.com>
2024-11-22 21:30:47 -0300

    feature: Criação inicial da lógica do Main e configuração do pom.xml

---
+++ src/main/java/com/marinaguimaraes/createUrlShortner/Main.java
@@ -0,0 +1,42 @@
+package com.marinaguimaraes.createUrlShortner;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Main implements RequestHandler<Map<String,Object>, Map<String, String>> {
+
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        String body = input.get("body").toString();
+
+
+
+        Map<String, String> bodyMap;
+        try{
+            bodyMap = objectMapper.readValue(body, Map.class);
+
+        }catch (Exception exception){
+            throw new RuntimeException("Error parsing Json body: " + exception.getMessage(), exception);
+        }
+
+        String originalUrl = bodyMap.get("originalUrl");
+        String expirationTime = bodyMap.get("expirationTime");
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0,8);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("code", shortUrlCode);
+
+
+
+        return response;
+    }
+}
