54efb4a6d002fb62ebd377261b19c8b492693f1d
Marina Guimarães <marinagvieira95@gmail.com>
2024-11-23 12:16:56 -0300

    feature: add amazon s3 for pom.xml, add class UrlData, configure s3 json

---
+++ src/main/java/com/marinaguimaraes/createUrlShortner/UrlData.java
@@ -0,0 +1,29 @@
+package com.marinaguimaraes.createUrlShortner;
+
+
+
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+
+    public UrlData(String originalUrl, long expirationTime) {
+        this.originalUrl = originalUrl;
+        this.expirationTime = expirationTime;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public long getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public void setExpirationTime(long expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+}
